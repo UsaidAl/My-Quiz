8e5d32555ca86ec0e5ce7c6824bf76d52d313c05
UsaidAl <ali_usaid98@hotmail.com>
2023-11-28 20:23:58 +0000

    Questions for quiz

---
+++ Assets/script.js
@@ -0,0 +1,28 @@
+const questions = [
+    {
+        question: 'What is the capital of France?',
+        options: ['Paris', 'Berlin', 'Madrid', 'Rome'],
+        correctAnswer: 'Paris'
+    },
+    {
+        question: 'Which programming language is this quiz about?',
+        options: ['Java', 'JavaScript', 'Python', 'C++'],
+        correctAnswer: 'JavaScript'
+    },
+    {
+        question: 'Which method is used to remove the last element from an array?',
+        options: ['pop()', 'shift()', 'remove()', 'delete()'],
+        correctAnswer: 'pop()'
+    },
+    {
+        question: 'What does the DOM stand for?',
+        options: ['Document Object Model', 'Data Object Model', 'Dynamic Object Model', 'Document Oriented Model'],
+        correctAnswer: 'Document Object Model'
+    },
+    {
+        question: 'What is the purpose of the "this" keyword in JavaScript?',
+        options: ['To refer to the current object', 'To create a new variable', 'To define a function', 'To represent the global scope'],
+        correctAnswer: 'To refer to the current object'
+    }
+    
+];
